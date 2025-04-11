c6b64b6d06eb248de04853ad8b24bccdd75b6363
Error-By-Night <36522345+Error-By-Night@users.noreply.github.com>
2025-04-11 14:44:59 -0700

    implement migraine logging api

--- backend/db/models/migraine.py
+++ backend/db/models/migraine.py
@@ -6,10 +6,13 @@ from sqlalchemy.orm import relationship
 from db.db_setup import Base
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream:backend/db/models/migraines.py
 class Migraines(Base):
     """
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 class Migraine(Base):
