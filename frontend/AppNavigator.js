628b88d6ddd7fe63c5f0326a95beb43b834866cc
Kai Tinder <ktinder2003@gmail.com>
2025-04-18 14:45:29 -0700

    multiselect, need api implementation

--- frontend/AppNavigator.js
+++ frontend/AppNavigator.js
@@ -6,6 +6,7 @@ import SignUpScreen from "./screens/SignUpScreen";
 import QuickLogScreen from "./screens/QuickLogScreen";
 import LoggingScreen from "./screens/LoggingScreen";
 import CalendarScreen from "./screens/CalendarScreen";
+import MultiSelectScreen from "./screens/MultiSelectScreen";
 
 const Stack = createStackNavigator();
 
@@ -13,11 +14,12 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Logging" component={LoggingScreen} />
         <Stack.Screen name="Calendar" component={CalendarScreen} />
         <Stack.Screen name="QuickLog" component={QuickLogScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="Logging" component={LoggingScreen} />
+        <Stack.Screen name="MultiSelect" component={MultiSelectScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
