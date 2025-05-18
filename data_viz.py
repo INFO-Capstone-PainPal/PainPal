0505487941c65345df98ad578d373e52d87581b3
kimeri21 <kimeri21@uw.edu>
2025-05-18 14:53:44 -0700

    viz function

--- data_viz.py
+++ data_viz.py
@@ -8,25 +8,27 @@ import plotly.graph_objects as go
 df = pd.read_csv('mig_data_test.csv')
 
 
-df['date'] = pd.to_datetime(df['date'])
+def plot_monthly_migraines(df):
+    df['date'] = pd.to_datetime(df['date'])
 
-df['month'] = df['date'].dt.to_period('M').astype(str)
-monthly_migraine_counts = df[df['Migraine'] == 1]['month'].value_counts().sort_index()
+    df['month'] = df['date'].dt.to_period('M').astype(str)
 
-month_names = pd.to_datetime(monthly_migraine_counts.index).strftime('%b')
+    monthly_migraine_counts = df[df['Migraine'] == 1]['month'].value_counts().sort_index()
 
-fig = go.Figure(data=[
-    go.Bar(
-        x=month_names,
-        y=monthly_migraine_counts.values,
-        marker_color="#665EA5"
+    month_names = pd.to_datetime(monthly_migraine_counts.index).strftime('%b')
+
+    fig = go.Figure(data=[
+        go.Bar(
+            x=month_names,
+            y=monthly_migraine_counts.values,
+            marker_color="#665EA5"
+        )
+    ])
+
+    fig.update_layout(
+        title="Number of Migraines per Month",
+        xaxis_title="Month",
+        yaxis_title="Number of Migraines"
     )
-])
 
-fig.update_layout(
-    title="Number of Migraines per Month",
-    xaxis_title="Month",
-    yaxis_title="Number of Migraines"
-)
-
-fig.show()
+    fig.show()
